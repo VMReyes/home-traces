a029d07d45d592dc8b95b45759d4d80f89c68101
Frances Hartwell <franceshartwell09@gmail.com>
2021-10-25 01:56:15 -0400

    add initial threading skeleton

--- gather_data.py
+++ gather_data.py
@@ -1,10 +1,42 @@
 import subprocess
+import concurrent.futures
 import tracerouteparser.tracerouteparser as traceparser
+import time
 
-if __name__ == "__main__":
+EXPERIMENT_TIME = 60
+MAX_WORKERS = 10
+
+sample_sites = [
+    "wikipedia.com"
+]
+
+
+
+def run_traceroute(sites):
     # run traceroute
     parser = traceparser.TracerouteParser()
-    proc = subprocess.check_output("traceroute google.com", shell=True, encoding='UTF-8')
-    out = proc
-    parser.parse_data(out)
-    print(parser)
+
+    start = time.time()
+    while time.time() < start + EXPERIMENT_TIME:
+        for site in sites:
+            proc = subprocess.check_output("traceroute {}".format(site), shell=True, encoding='UTF-8')
+            out = proc
+            parser.parse_data(out)
+            print(parser)
+        time.sleep(10)
+    return parser
+
+if __name__ == "__main__":
+    #run_traceroute(sample_sites)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        num_sites, leftover = divmod(len(sample_sites), MAX_WORKERS)
+        sites = [sample_sites[i*num_sites+min(i, leftover):(i+1)*num_sites+min(i+1, leftover)] for i in range(MAX_WORKERS)]
+
+        futures = {executor.submit(run_traceroute, sub_sites): sub_sites for sub_sites in sites}
+        for future in concurrent.futures.as_completed(futures):
+            tested = futures[future]
+            try:
+                future.result()
+            except Exception as exc:
+                print("something went wrong :(")
+                raise exc
