346d64d04ca06c30c0c3a9e779ab74250fd43750
Victor Reyes <vmreyes@mit.edu>
2021-11-17 18:15:25 -0500

    maxed traceroute hops to 30

--- gather_data_for_users.py
+++ gather_data_for_users.py
@@ -4,7 +4,10 @@ import csv
 import os
 import sys
 
-EXPERIMENT_TIME = time.time_ns()
+from gather_data import EXPERIMENT_TIME
+
+EXPERIMENT_START_TIME = int(time.time())
+EXPERIENT_LENGTH_HOURS = 8
 START_TIME = 10000000
 OUTPUT_PATH = 'output/'
 
@@ -26,11 +29,11 @@ def setup_output_folder():
 
 def run_traceroute(site):
     if sys.platform == "win32":
-        proc = subprocess.check_output("tracert {}".format(site), shell=True, encoding='UTF-8')
+        proc = subprocess.check_output("tracert -h 30 {}".format(site), shell=True, encoding='UTF-8')
     elif sys.platform == "linux" or sys.platform == "linux2":
-        proc = subprocess.check_output("traceroute -A -q 1 {}".format(site), shell=True, encoding='UTF-8')
+        proc = subprocess.check_output("traceroute -A -q 1 -m 30 {}".format(site), shell=True, encoding='UTF-8')
     elif sys.platform == "darwin":
-        proc = subprocess.check_output("traceroute -a -q 1 {}".format(site), shell=True, encoding='UTF-8', stderr=subprocess.STDOUT)
+        proc = subprocess.check_output("traceroute -a -q 1 -m 30 {}".format(site), shell=True, encoding='UTF-8', stderr=subprocess.STDOUT)
     else:
         print("Unknown Operating System.")
         assert False
@@ -40,14 +43,14 @@ if __name__ == "__main__":
     setup_output_folder()
     sites = load_sites()
     if(time.time() >= START_TIME):
-        while (True):
+        while (time.time() < EXPERIMENT_START_TIME + EXPERIENT_LENGTH_HOURS * 60 * 6):
             for site in sites:
                 timestamp = time.time()
                 try:
                     proc = run_traceroute(site)
                 except:
                     continue
-                with open('output/results-' +str(EXPERIMENT_TIME)+'.csv', 'a') as outputfile:
+                with open('output/results-' +str(EXPERIMENT_START_TIME)+'.csv', 'a') as outputfile:
                     outputwriter = csv.writer(outputfile)
                     outputwriter.writerow([timestamp, proc])
             time.sleep(60*30)
