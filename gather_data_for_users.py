97cb8103df7b640391322a34fe743f8a34bb3c65
Victor Reyes <vmreyes@mit.edu>
2021-11-17 17:14:35 -0500

    added script for users to run

---
+++ gather_data_for_users.py
@@ -0,0 +1,48 @@
+import subprocess
+import time
+import csv
+import os
+import sys
+
+EXPERIMENT_TIME = time.time_ns()
+START_TIME = 10000000
+OUTPUT_PATH = 'output/'
+
+def load_sites():
+    sites = []
+    with open('data/top50sites.csv') as sitesfile:
+        sitereader = csv.reader(sitesfile)
+        for row in sitereader:
+            site = row[0]
+            sites.append(site)
+    return sites
+
+def setup_output_folder():
+    outputExists = os.path.exists(OUTPUT_PATH)
+
+    if not outputExists:
+        os.makedirs(OUTPUT_PATH)
+        print("Created output directory")
+
+def run_traceroute(site):
+    if sys.platform == "win32":
+        proc = subprocess.check_output("tracert {}".format(site), shell=True, encoding='UTF-8')
+    else:
+        proc = subprocess.check_output("traceroute -A -q 1 {}".format(site), shell=True, encoding='UTF-8')
+    return proc
+
+if __name__ == "__main__":
+    setup_output_folder()
+    sites = load_sites()
+    if(time.time() >= START_TIME):
+        while (True):
+            for site in sites:
+                timestamp = time.time()
+                try:
+                    proc = run_traceroute(site)
+                except:
+                    continue
+                with open('output/results-' +str(EXPERIMENT_TIME)+'.csv', 'a') as outputfile:
+                    outputwriter = csv.writer(outputfile)
+                    outputwriter.writerow([timestamp, proc])
+
