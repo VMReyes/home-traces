c6f4c373e2cd5adf878a0492b21ce8170efa17b7
Frances Hartwell <franceshartwell09@gmail.com>
2021-12-01 19:44:01 -0500

    add combine files to single df function to parse_output

--- parse_output.py
+++ parse_output.py
@@ -1,4 +1,6 @@
 import trparse
+import os
+import time
 import pandas as pd
 
 def trparse_tree_to_record(trparse_trees, max_hops=30, num_probes=1):
@@ -41,6 +43,38 @@ def parse_output(path):
     traceroute_df = trparse_tree_to_record(trparse_trees)
     traceroute_df['time'] = output_df['time']
     return traceroute_df
+
+def combine_data(file_dir=None, outfile=None):
+    '''
+    Uses pandas to load all csv files in a folder and then concatanates them together. Optionally
+    saves the concatanated dataframe as a parquet file.
+
+    file_dir: Directory to read csv files from. Defaults to './output/final' 
+    outfile: Filename to write concatanated df parquet to. Located in file_dir. 
+    '''
+    if file_dir is None:
+        file_dir = './output/final'
+
+    dfs = {}
+    for f in os.listdir(file_dir):
+        df = parse_output(os.path.join(file_dir, f))
+        df['time'] = pd.to_datetime(df['time'].apply(lambda x: time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(x))))
+        df['source'] = f[:-4]
+        dfs[f[:-4]] = df
+        print(f, 'done')
+
+    # Home Cleveland got split into two files, join them together and rename the source column
+    dfs['home-cleveland'] = dfs['home-cleveland-1'].append(dfs['home-cleveland-2']).reset_index(drop=True)
+    dfs['home-cleveland']['source'] = 'home-cleveland'
+    del dfs['home-cleveland-1']; del dfs['home-cleveland-2']
+
+    df = pd.concat(dfs.values(), ignore_index=True)
+
+    if outfile is not None:
+        df.to_parquet(os.path.join(file_dir, outfile))
+
+    return df
+
 
 if __name__ == "__main__":
     path = "data/results-1637199367-linux.csv" # Change on your machine
